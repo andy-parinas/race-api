c0eec063f66130770509f7268be164101663b0e5
Andy Parinas <andy@budgetpetproducts.com.au>
2023-05-29 21:24:22 +1000

    working race analysis

--- app/api/v1/analysis.py
+++ app/api/v1/analysis.py
@@ -22,10 +22,9 @@ def analyse_race_advance(analysis_in: AnalsyisInput,  db: Session = Depends(get_
     results, race_horses = __analyze_multiple_race(
         db, analysis_in.race_ids, preferences, analysis_in.preference_type)
 
-    __get_analysis_results_values(db, results, race_horses)
-    # print(results)
+    results = __get_analysis_results_values(db, results, race_horses)
 
-    return "results"
+    return results
 
 
 def __analyze_multiple_race(db, race_ids, preferences, preference_type):
@@ -75,27 +74,23 @@ def __get_analysis_results_values(db, results, race_horses: List[Dict[int, List[
 
     horse_rating = results['rating']
 
+    output = []
+
     for horse_id, rating in sorted(horse_rating.items(), key=lambda x: x[1], reverse=True):
         for race in race_horses:
             for race_id, horse_ids in race.items():
                 if horse_id in horse_ids:
-                    # horse_race_info
+                    horse_race_info = repo.horse_race_info.get_horse_race_info(
+                        db, race_id=race_id, horse_id=horse_id)
+                    if horse_race_info:
+                        output.append({
+                            "rating": rating,
+                            "details": horse_race_info
+                        })
 
-                    race_name = f"Race {race_id}"
-                    print(
-                        f"Horse {horse_id} belongs to {race_name} with a rating of {rating}")
                     break
 
-    # result_ratings = results['rating']
-    # for race in race_horses:
-    #     for race_id, horse_ids in race.items():
-    #         for horse_id in horse_ids:
-
-    #             if horse_id in result_ratings:
-    #                 race_number = f"Race {race_id}"
-    #                 horse_rating_value = result_ratings[horse_id]
-    #                 print(
-    #                     f"Horse {horse_id} belongs to {race_number} with a rating of {horse_rating_value}")
+    return output
 
 
 def __analyze_single_race(db, race_id, preferences, preference_type):
@@ -119,37 +114,6 @@ def __convert_stat_to_df(stat_query_results):
     return DataFrame(race_stat_data)
 
 
-def _get_analysis_from_races(db: Session, races_ids, preferences: List[str], preference_type):
-    final_df = DataFrame()
-
-    preferences_with_all = preferences.copy()
-    preferences_with_all.append("all")
-
-    for race_id in races_ids:
-
-        df = repo.current_race.get_race_dataframe(
-            db, preferences_with_all, race_id)
-        if df.empty:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="Races not found")
-
-        analysis_results = _get_bayes_results(df, preferences, preference_type)
-
-        if not analysis_results:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                                detail="No results found. Statistics does not have enough information.")
-
-        analysis_df = DataFrame.from_dict(
-            analysis_results, orient='index', columns=['rating'])
-        analysis_df.index.name = 'horse_id'
-
-        final_df = concat([final_df, analysis_df], ignore_index=False)
-
-    final_df = final_df.sort_values(['rating'], ascending=[False])
-    top_horses = final_df.head(5).to_dict()
-    return __get_horses_from_analysis(db, top_horses['rating'])
-
-
 def __get_bayes_results(df: DataFrame, selected_preferences: List[str], preference_type):
 
     bayes = BayseAnalysis(df=df, prefrerences=selected_preferences,
@@ -160,23 +124,3 @@ def __get_bayes_results(df: DataFrame, selected_preferences: List[str], preferen
 
     # return likelihood
     return bayes.get_probability(data, likelihood)
-
-
-# def __get_horses_from_analysis(db: Session, horses_dict):
-#     horse_ids = list(horses_dict.keys())
-#     horses = repo.horse.get_horses_from_ids(db, ids=horse_ids)
-#     horse_array = []
-#     for horse in horses:
-#         horse_array.append({
-#             "id": horse.id,
-#             "horse_id": horse.horse_id,
-#             "horse_name": horse.horse_name,
-#             "rating": horses_dict[horse.id],
-#             "race_id": horse.race_id,
-#             "race_number": horse.race.race_number,
-#             "meeting": horse.race.meeting.track_name,
-#             "date": horse.race.meeting.meeting_date,
-#             "state": horse.race.meeting.state
-#         })
-
-#     return sorted(horse_array, key=lambda d: d['rating'], reverse=True)
